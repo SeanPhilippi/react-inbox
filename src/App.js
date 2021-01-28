5a9a7559baad6fae852685a2149a5d5831f66b9e
SeanPhilippi <sean.philippi@gmail.com>
2021-01-27 19:29:49 -0600

    adding web3 to project and using provider from metamask injected into browser window

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
+import web3 from './web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    web3.eth.getAccounts()
+      .then(console.log);
+
+      return (
+        <div></div>
+      )
+  }
+
 }
 
 export default App;
