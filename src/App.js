9c9eff9b67f3e3d3d94733145d3df3db6a05e9d0
SeanPhilippi <sean.philippi@gmail.com>
2021-01-31 13:28:08 -0600

    setting and displaying state values

--- src/App.js
+++ src/App.js
@@ -6,20 +6,29 @@ import lottery from './lottery';
 class App extends PureComponent {
   state = {
     manager: '',
+    players: [],
+    balance: '',
   };
 
   async componentDidMount() {
     // from prop where accounts[0] would be specified in call() no longer necessary
     // when metamask provider is being used. default account is already set to first account
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
   }
 
   render() {
+    const { manager, players, balance } = this.state;
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p className='manager'>This contract is manged by {this.state.manager}</p>
+        <p className='manager'>
+          This contract is manged by {manager}. There are currently {players.length} people entered,
+          competing to win {web3.utils.fromWei(balance, 'ether')}
+        </p>
       </div>
     );
   }
